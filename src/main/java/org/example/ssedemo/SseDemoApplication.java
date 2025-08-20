dcc184ce89c6621dbb45f52539d425cbafaa705a
Sam Lu <luxiaocong524@163.com>
2025-08-20 19:43:45 +0800

    [feature] add sse demo method

--- src/main/java/org/example/ssedemo/SseDemoApplication.java
+++ src/main/java/org/example/ssedemo/SseDemoApplication.java
@@ -2,12 +2,44 @@ package org.example.ssedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.time.LocalTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@RestController
 @SpringBootApplication
 public class SseDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SseDemoApplication.class, args);
+    }
+
+    @GetMapping("/stream-sse-mvc")
+    public SseEmitter streamSseMvc() {
+        SseEmitter emitter = new SseEmitter(0L);
+        emitter.onCompletion(() -> System.out.println("Completed"));
+        emitter.onTimeout(() -> System.out.println("Timeout"));
+        emitter.onError(ex -> System.out.println("Error"));
+        ExecutorService sseMvcExecutor = Executors.newSingleThreadExecutor();
+        sseMvcExecutor.execute(() -> {
+            try {
+                for (int i = 0; true; i++) {
+                    SseEmitter.SseEventBuilder event = SseEmitter.event()
+                            .data("SSE MVC - " + LocalTime.now().toString())
+                            .id(String.valueOf(i))
+                            .name("sse event - mvc");
+                    emitter.send(event);
+                    Thread.sleep(1000);
+                }
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
+            }
+        });
+        return emitter;
     }
 
 }
