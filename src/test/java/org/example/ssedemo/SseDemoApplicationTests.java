6dcb29e08e64115a928e225ad6131f64d830cfe7
Sam Lu <luxiaocong524@163.com>
2025-08-20 19:42:23 +0800

    first commit

---
+++ src/test/java/org/example/ssedemo/SseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ssedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
